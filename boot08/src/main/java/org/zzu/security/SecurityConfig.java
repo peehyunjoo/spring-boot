c75438c59cf8d7d66a1707279b128be91328428b
peehyunjoo <peehyunjoo@gmail.com>
2018-10-07 18:06:23 +0900

    security

--- boot08/src/main/java/org/zzu/security/SecurityConfig.java
+++ boot08/src/main/java/org/zzu/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.zzu.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,13 @@ import lombok.extern.java.Log;
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+//	@Autowired
+//	DataSource dataSource;
+	
+	@Autowired
+	ZzuUsersService zzuUsersService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		log.info("security config....");
@@ -21,17 +30,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.formLogin().loginPage("/login");
 		http.exceptionHandling().accessDeniedPage("/accessDenied");
 		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+		http.userDetailsService(zzuUsersService);
 	}
 	
-	@Autowired
+/*	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
 		log.info("build Auth global...");
+		
+		String query1 = "SELECT uid username, CONCAT('{noop}', upw) password, true enabled FROM"
+						+ " tbl_members WHERE uid= ?";
+		String query2 = "SELECT member uid, role_name role FROM tbl_member_roles WHERE member =? ";
+		
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(query1)
+		.rolePrefix("ROLE_")
+		.authoritiesByUsernameQuery(query2);
 		
 		auth.inMemoryAuthentication().
 		withUser("manager").
 		password("{noop}1111").
 		roles("MANAGER");
-	}
+	}*/
 }
 
 
---
+++ boot08/src/main/java/org/zzu/security/ZzuSecurityUser.java
@@ -0,0 +1,43 @@
+package org.zzu.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.zzu.domain.Member;
+import org.zzu.domain.MemberRole;
+
+import antlr.MakeGrammar;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ZzuSecurityUser extends User {
+	
+	private static final String ROLE_PREFIX = "ROLE_";
+	private Member member;
+	
+	public ZzuSecurityUser(Member member) {
+		super(member.getUid(), "{noop}" +member.getUpw(), makeGrantedAuthority(member.getRoles()));
+		this.member = member;
+	}
+	
+	private static List<GrantedAuthority> makeGrantedAuthority(List<MemberRole> roles){
+		List<GrantedAuthority> list = new ArrayList<>();
+		roles.forEach(
+			role -> list.add(
+					new SimpleGrantedAuthority(ROLE_PREFIX + role.getRoleName())));
+					return list;
+	
+	}
+	public ZzuSecurityUser(
+		String username,
+		String password,
+		Collection<? extends GrantedAuthority> authorities){
+			super(username, password, authorities);
+	}
+}
