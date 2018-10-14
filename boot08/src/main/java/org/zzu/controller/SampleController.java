3718dd5e9c38433a4f9ee61167382a019481a3a5
peehyunjoo <peehyunjoo@gmail.com>
2018-10-14 17:25:53 +0900

    security password encoding

--- boot08/src/main/java/org/zzu/controller/SampleController.java
+++ boot08/src/main/java/org/zzu/controller/SampleController.java
@@ -1,5 +1,6 @@
 package org.zzu.controller;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,19 @@ import lombok.extern.java.Log;
 @Controller
 @Log
 public class SampleController {
+
+	@Secured({"ROLE_ADMIN"})
+	@RequestMapping("/adminSecret")
+	public void forAdminSecret() {
+		log.info("admin secret ");
+	}
 	
+	@Secured({"ROLE_MANAGER"})
+	@RequestMapping("/managerSecret")
+	public void forManagerSecret() {
+		log.info("manger secret");
+	}
+
 	@GetMapping("/")
 	public String index() {
 		log.info("index");
--- boot08/src/main/java/org/zzu/security/SecurityConfig.java
+++ boot08/src/main/java/org/zzu/security/SecurityConfig.java
@@ -3,19 +3,26 @@ package org.zzu.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import lombok.extern.java.Log;
 
 @Log
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
-//	@Autowired
-//	DataSource dataSource;
+	@Autowired
+	DataSource dataSource;
 	
 	@Autowired
 	ZzuUsersService zzuUsersService;
@@ -30,7 +37,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.formLogin().loginPage("/login");
 		http.exceptionHandling().accessDeniedPage("/accessDenied");
 		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
-		http.userDetailsService(zzuUsersService);
+		//http.userDetailsService(zzuUsersService);
+		http.rememberMe().key("zzu").userDetailsService(zzuUsersService)
+		.tokenRepository(getJDBCRepository())
+		.tokenValiditySeconds(60*60*24);
+	
+	}
+	
+	private PersistentTokenRepository getJDBCRepository() {
+		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+		repo.setDataSource(dataSource);
+		return repo;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		log.info("build Auth global....");
+		
+		auth.userDetailsService(zzuUsersService).passwordEncoder(passwordEncoder());
 	}
 	
 /*	@Autowired
